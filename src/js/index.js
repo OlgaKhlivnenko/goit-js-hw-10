e1f1d81b66cfb3e6a953d2c2ee6d57908066653b
OlgaKhlivnenko <xlivnenko@ukr.net>
2021-12-11 04:55:35 -0500

    11/12

--- src/js/index.js
+++ src/js/index.js
@@ -1,34 +1,37 @@
 import '../css/styles.css';
-import { debounce } from "lodash.debounce";
+
 // import API from './fetchname';
 import Notiflix from 'notiflix';
 
-
+const debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
 const inputEl = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
-inputEl.addEventListener(`input`, onInputChange);
+inputEl.addEventListener(`input`, debounce((onInputChange), DEBOUNCE_DELAY));
 
 function onInputChange(evt) {
-    const input = evt.currentTarget.value;
-    console.log(`input`, input);
-    fetchCountries(input).then(renderCountryCard);
+    let inputValue = evt.target.value;
+    console.log(inputValue);
+    let country = inputValue.trim();
+    console.log(country);
+    fetchCountries(country).then(renderCountryCard);
 }
 function fetchCountries(name) {
-    
-    return fetch('https://restcountries.com/v2/name/${name}')
+    console.log(name);
+     return fetch('https://restcountries.com/v3.1/name/${name}')
         .then(response =>  response.json() );
 }
 
-function renderCountryCard(name) {
-     if (name.length > 10) {
+function renderCountryCard(country) {
+    console.log(country);
+     if (country.length > 10) {
         console.log(`10`);
         Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
     }
-    if (name.length >= 2 && name.length < 10) {
+    if (country.length >= 2 && country.length < 10) {
          console.log(`2`);
-        countryList.innerHTML = name
+        countryList.innerHTML = country
             .map(({ flags, name, }) =>
                 `<li>
                     <svg class="flag__image" width="40px" height="40px">
@@ -40,9 +43,8 @@ function renderCountryCard(name) {
             .join('');
         return;
     }
-    if (name.length === 1) {
-        console.log(`1`);
-        countryInfo.innerHTML = name
+    if (country.length === 1) {
+        countryInfo.innerHTML = country
             .map(({ flags, name, capital, population, languages, }) => 
                  `<svg class="flag__image" width="40px" height="40px">
                         <use href="${flags.svg}"></use>
